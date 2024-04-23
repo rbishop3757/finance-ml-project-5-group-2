9388fbbd7e0bd24d0a31a1324cebd1b3c3f30acd
rider <bishop.r@northeastern.edu>
2024-04-22 20:51:56 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,39 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import React, { useState } from "react";
-import { TextField, FormControl, Button } from "@mui/material";
+import { TextField, FormControl, Button, Box } from "@mui/material";
+
+function DataInput({ label, field, setField }) {
+  return (
+    <TextField
+      style={{
+        margin: "1rem 0 1rem 0",
+      }}
+      label={label}
+      type="number"
+      variant="outlined"
+      onChange={(e) => setField(e.target.value)}
+      value={field}
+    />
+  );
+}
 
 export default function Home() {
   const [field1, setField1] = useState("");
   const [field2, setField2] = useState("");
   const [field3, setField3] = useState("");
+  const [field4, setField4] = useState("");
+  const [field5, setField5] = useState("");
+  const [field6, setField6] = useState("");
+  const [field7, setField7] = useState("");
+  const [field8, setField8] = useState("");
+  const [field9, setField9] = useState("");
+  const [field10, setField10] = useState("");
+  const [field11, setField11] = useState("");
+  const [field12, setField12] = useState("");
+  const [field13, setField13] = useState("");
+  const [field14, setField14] = useState("");
+  const [field15, setField15] = useState("");
 
   const handleSubmit = (event) => {
     console.log("got here");
@@ -29,35 +56,58 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to FINA4390 Group 2 Project 5!" />
-        <FormControl style={{ width: "20rem" }}>
-          <TextField
-            style={{
-              margin: "1rem 0 1rem 0",
+        <Header title="Welcome to Group 2's Bankruptcy's Calculator" />
+        <FormControl style={{ width: "70rem" }}>
+          <Box
+            sx={{
+              display: "grid",
+              gridTemplateColumns: "repeat(3, 3fr)",
+              columnGap: 3,
+              rowGap: 1,
+              mb: "2rem",
             }}
-            label="Field 1"
-            variant="outlined"
-            onChange={(e) => setField1(e.target.value)}
-            value={field1}
-          />
-          <TextField
-            style={{
-              margin: "1rem 0 1rem 0",
-            }}
-            label="Field 2"
-            variant="outlined"
-            onChange={(e) => setField2(e.target.value)}
-            value={field2}
-          />
-          <TextField
-            style={{
-              margin: "1rem 0 1rem 0",
-            }}
-            label="Field 3"
-            variant="outlined"
-            onChange={(e) => setField3(e.target.value)}
-            value={field3}
-          />
+          >
+            <DataInput label={"LABEL 1"} field={field1} setField={setField1} />
+            <DataInput label={"LABEL 2"} field={field2} setField={setField2} />
+            <DataInput label={"LABEL 3"} field={field3} setField={setField3} />
+            <DataInput label={"LABEL 4"} field={field4} setField={setField4} />
+            <DataInput label={"LABEL 5"} field={field5} setField={setField5} />
+            <DataInput label={"LABEL 6"} field={field6} setField={setField6} />
+            <DataInput label={"LABEL 7"} field={field7} setField={setField7} />
+            <DataInput label={"LABEL 8"} field={field8} setField={setField8} />
+            <DataInput label={"LABEL 9"} field={field9} setField={setField9} />
+            <DataInput
+              label={"LABEL 10"}
+              field={field10}
+              setField={setField10}
+            />
+            <DataInput
+              label={"LABEL 11"}
+              field={field11}
+              setField={setField11}
+            />
+            <DataInput
+              label={"LABEL 12"}
+              field={field12}
+              setField={setField12}
+            />
+            <DataInput
+              label={"LABEL 13"}
+              field={field13}
+              setField={setField13}
+            />
+            <DataInput
+              label={"LABEL 14"}
+              field={field14}
+              setField={setField14}
+            />
+            <DataInput
+              label={"LABEL 15"}
+              field={field15}
+              setField={setField15}
+            />
+          </Box>
+
           <Button
             variant="outlined"
             color="secondary"
